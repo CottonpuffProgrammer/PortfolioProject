40913f481624005467a46183c37f7d0a75c23aa3
Isaac Detert <detert7392@students.cptc.edu>
2025-02-21 04:52:15 -0800

    Added SectionType to Section object and updated database, standardized comments across objects and classes, added button to add data for testing.

--- PortfolioProject/Models/Section.cs
+++ PortfolioProject/Models/Section.cs
@@ -2,28 +2,25 @@
 
 namespace PortfolioProject.Models
 {
-    /// <summary>
-    /// Represents a singular section
-    /// </summary>
+    // Represents a singular section
     public class Section
     {
-        /// <summary>
-        /// Uniquely identifies a section
-        /// </summary>
+        // Uniquely identifies a section
         [Key]
         public string SectionId { get; set; }
 
-        /// <summary>
-        /// The internal name of the section, will help provide
-        /// a working clickable link when displayed on a webpage
-        /// </summary>
+        // The coding language that a section
+        // is from, helps the View Model send correct data
+        [Required]
+        public string SectionType { get; set; }
+
+        // The internal name of the section, will help provide
+        // a working clickable link when displayed on a webpage
         [Required]
         public string SectionName { get; set; }
 
-        /// <summary>
-        /// The name of the section that is displayed on
-        /// a webpage
-        /// </summary>
+        // The name of the section that is displayed on
+        // a webpage
         [Required]
         public string SectionDisplay { get; set; }
     }
--- PortfolioProject/Models/SectionAndBookmarkViewModel.cs
+++ PortfolioProject/Models/SectionAndBookmarkViewModel.cs
@@ -1,23 +1,17 @@
 ﻿namespace PortfolioProject.Models
 {
-    /// <summary>
-    /// Used to send a set of objects to 
-    /// webpages for display
-    /// </summary>
-    public class ViewPhotosViewModel
+    // Used to send a set of objects to 
+    // webpages for display
+    public class SectionsBookmarksViewModel
     {
-        /// <summary>
-        /// Contains a list of Section objects to 
-        /// be sent to a view, these will be 
-        /// organized in the SectionsController
-        /// </summary>
+        // Contains a list of Section objects to 
+        // be sent to a view, these will be 
+        // organized in the SectionsController
         public List<Section>? Sections { get; set; }
 
-        /// <summary>
-        /// Contains a list of Bookmark objects to 
-        /// be sent to a view, these will be 
-        /// organized in the SectionsController
-        /// </summary>
+        // Contains a list of Bookmark objects to 
+        // be sent to a view, these will be 
+        // organized in the SectionsController
         public List<Bookmark>? Bookmarks { get; set; }
     }
 }
